654cf1ce72fabeab04077d228c4f0b3ef0894819
Fubarius Jenkins III <fubarius9180@gmail.com>
2016-05-11 00:28:17 -0400

    Add nav link to home.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # Wraps link_to in an li tag
+  # and adds class="active" if linking to the current page.
+  def nav_link(*args, &block)
+    content_tag(:li, class: ('active' if current_page?(args[1]))) do
+      link_to(*args, &block)
+    end
+  end
 end
