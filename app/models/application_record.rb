c357773042dd620cc9fe092e14908213bce7dea0
Fubarius Jenkins III <fubarius9180@gmail.com>
2016-05-13 14:50:20 -0400

    Add set_user before_validation to automatically set the user association to User.current on new objects.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  before_validation :set_user, on: :create
+
+  private
+  def set_user
+    self.user ||= User.current if self.respond_to?(:user)
+  end
 end
